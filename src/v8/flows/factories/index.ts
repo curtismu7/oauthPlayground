544fc70eb985c0c929da7ae47654ca9d576acf2b
Curtis Muir <49688644+curtismu7@users.noreply.github.com>
2026-06-19 08:18:14 -0500

    chore(dead-code): remove orphaned v7/v8/v8u files (no live importers)

--- src/v8/flows/factories/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file index.ts
- * @module v8/flows/factories
- * @description Factory exports
- */
-
-export { MFAFlowComponentFactory } from './MFAFlowComponentFactory';
-export type { ControllerFactoryConfig } from './MFAFlowControllerFactory';
-export { MFAFlowControllerFactory } from './MFAFlowControllerFactory';
